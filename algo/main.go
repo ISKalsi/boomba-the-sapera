31eaacadf52fe49cda00f31005f807b8deba5b3e
pavandubey <ikroop.singh.kalsi@gmail.com>
2021-07-28 03:03:26 +0530

    v1.6.3 - [!PANIC!] code going wrong, disable "dontBlockTail" flag.

--- algo/main.go
+++ algo/main.go
@@ -179,7 +179,7 @@ func (a *Algorithm) NextMove(gr *models.GameRequest) string {
 
 	a.SetNewStart(gr.You.Head)
 	a.SetNewDestination(gr.You.Body[len(gr.You.Body)-1])
-	a.dontBlockTail = true
+	a.dontBlockTail = false
 
 	if a.longestPath() {
 		return a.getDirection(a.solvedPath[0])
